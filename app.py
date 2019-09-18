35083e71277be0175be24edf9b6fce4108531008
Karl Hobley <karl@kaed.uk>
2019-09-18 13:46:36 +0100

    Make it more robust

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def generate_pdf():
     if not request.json:
         abort(400)
 
+    for required_field in ['html', 'base_url', 'filename', 's3_upload']:
+        if required_field not in request.json:
+            abort(400)
+
     output = BytesIO()
     HTML(string=request.json['html'], base_url=request.json['base_url']).write_pdf(output)
 
@@ -18,11 +22,14 @@ def generate_pdf():
     response = requests.post(request.json['s3_upload']['url'], data=request.json['s3_upload']['fields'], files={'file': (request.json['filename'], output.getvalue())})
 
     # Ping callback URL
-    requests.post(request.json['callback_url'], json={
-        'request_id': request.json['request_id'],
-        'filename': request.json['filename'],
-        'filesize': len(output.getvalue()),
-    })
+    if 'callback_url' in request.json:
+        data = {
+            'filename': request.json['filename'],
+            'filesize': len(output.getvalue()),
+        }
+        if 'request_id' in request.json:
+            data['request_id'] = request.json['request_id']
+        requests.post(request.json['callback_url'], json=data)
 
 if __name__ == "__main__":
     app.run()
